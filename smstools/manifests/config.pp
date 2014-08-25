56e6159d869dc3a7cb43d56a519a26b703b74625
“Lester <lester@applester.co>
2014-08-25 21:44:20 +0800

    added /etc/smsd.conf
    config file template

--- smstools/manifests/config.pp
+++ smstools/manifests/config.pp
@@ -3,4 +3,13 @@
 # This class is called from smstools
 #
 class smstools::config {
+  
+  file { '/etc/smsd.conf':
+    ensure  => file,
+    owner   => 0,
+    group   => 0,
+    mode    => '0644',
+    content => template('smstools/smsd.conf.erb'),
+  }
+
 }
